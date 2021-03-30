41cd5f35ca438fa0cd76cd7e4ec5f6ad0c904b5c
Matus Giertl <matus1giertl@gmail.com>
2021-03-30 15:00:33 +0200

    first commit

---
+++ Kofi/KofiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KofiApp.swift
+//  Kofi
+//
+//  Created by Giertl Matus on 30.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct KofiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
